1482e61cfc8379c40057888a403024da6f65db89
quangChe <quang.n.che@gmail.com>
2019-07-28 00:00:57 -0700

    Restyled and built out basic layout with input and submit button

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform, 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  state = {
+    text: '',
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  changeText = (text) => {
+    this.setState({text})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput style={{width: 300, padding: 10}}
+          placeholder='Enter text'
+          onChangeText={this.changeText}
+          value={this.state.text}/>
+        <Button title='Add'/>
+
       </View>
     );
   }
@@ -32,18 +33,14 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
+    paddingTop: Platform.OS === 'ios' ? 50 : 0,
+  }, 
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
